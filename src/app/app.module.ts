a847212320486b8faa1405ae3fa20acf66923519
Jefte <jeftekerison@gmail.com>
2024-03-08 11:32:13 -0300

    ajuste no module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -9,6 +10,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DesignComponent } from './pages/design/design.component';
 import { WebdevComponent } from './pages/webdev/webdev.component';
+
 
 @NgModule({
   declarations: [
